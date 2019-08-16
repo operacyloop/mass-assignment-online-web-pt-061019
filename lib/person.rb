f9eb6c61f20462104c725817123f3ab8bb4db30e
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-16 23:58:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,10 +5,11 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
               :wrist_size, :glove_size, :pant_length, :pant_width
   
 def initialize (attributes)
-attributes.each do |key, value|
-self-send("#{key}=")
-
-# end
+  attributes.each do |key, value|
+    self.send("#{key}="), value)
+  end
+end
+end
 
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
